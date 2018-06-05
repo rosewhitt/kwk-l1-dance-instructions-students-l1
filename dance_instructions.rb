f6ff2d79571fd5ffda4995c4418dc36af5298c58
rosewhitt <rosewhitt@icloud.com>
2018-06-05 15:16:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,27 @@
+def gangnam_style
+  puts "You are about to learn to dance Gangnam Style!"
+end
 
+
+def proper_stance
+  puts "Plant your legs far apart."
+  puts "Bend your knees slightly."
+  puts "Keep your posture loose."
+  puts "Time to bust a move!"
+end
+
+
+def base_footwork
+  puts "Lift yor right foot."
+  puts "Return your right foot to the ground."
+  puts "Finish with a small skip-step backwards."
+end
+
+def skip_step_left
+  puts "Lower your left foot to the ground."
+  puts "Bounce your left foot back up slightly, kicking it a few inches back."
+  puts "Left, Right, Left, Left"
+end
+
+def 
 # Code your methods below
